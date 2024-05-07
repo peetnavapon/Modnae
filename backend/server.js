d46ae72a1f6b5fff698bd2ad8451ee0916c53f99
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2024-05-07 16:48:14 +0700

    server created

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+mongoose.connect(
+  "mongodb+srv://modnoy:modnaetuanoy@modnae.olhb5sg.mongodb.net/modnaeDB"
+);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB database connected.");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
