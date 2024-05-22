5befc00e5423bca519f135d0b67f9d3930002fa4
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2024-05-22 21:19:37 +0700

    fix login, register

--- src/app/auth/login.jsx
+++ src/app/auth/login.jsx
@@ -6,7 +6,8 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "../../components/navbar";
-import "./login.css"
+import loginimg from "../../assets/login.png";
+import "./login.css";
 
 export function Login() {
   const dispatch = useDispatch();
@@ -59,43 +60,67 @@ export function Login() {
   };
   return (
     <>
-    <Navbar/>
-    <div className="loginCenter">
-      <div className="loginContent">
-        <div className="loginPicture">
+      <div className="loginCenter">
+        <div className="loginContent">
+          <div className="loginPicture">
+            <img src={loginimg} alt="" />
+          </div>
 
-        </div>
+          <div className="loginMain">
+            <h1 className="headLogin">เข้าสู่ระบบ</h1>
+            <form className="loginForm" onSubmit={handleSubmit}>
+              <div className="inputContainer">
+                <svg
+                  className="inputIcon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+                </svg>
+                <input
+                  className="loginInput"
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  placeholder="อีเมล"
+                />
+              </div>
 
-        <div className="loginMain">
-          <h1 className="headLogin">Sign In</h1>
-          <form className="loginForm" onSubmit={handleSubmit}>
+              <div className="inputContainer">
+                <svg
+                  className="inputIcon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" />
+                </svg>
+                <input
+                  className="loginInput"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  placeholder="รหัสผ่าน"
+                />
+              </div>
 
-            <div className="inputContainer">
-              <svg className="inputIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
-              <input className="loginInput" type="email" name="email" onChange={handleChange} placeholder="Email"/>
-            </div>
+              <p>{log}</p>
+              <button className="loginButton">เข้าสู่ระบบ</button>
+              <p>
+                ยังไม่มีบัญชี?{" "}
+                <Link to="/register" className="toRegister">
+                  สร้างบัญชี
+                </Link>
+              </p>
+            </form>
 
-
-            <div className="inputContainer">
-              <svg className="inputIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg>
-              <input className="loginInput" type="password" name="password" onChange={handleChange} placeholder="Password"/>
-            </div>
-
-            <p>{log}</p>
-            <button className="loginButton">Sign in</button>
-            <p>Don’t have an account ? <Link to="/register" className="toRegister">Sign Up</Link></p>
-          </form>
-          
-          {/* <button className="toRegister">
+            {/* <button className="toRegister">
             <Link to="/register">
               Register
             </Link>
           </button> */}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
