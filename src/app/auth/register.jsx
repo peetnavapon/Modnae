e1b02a12ba136fa8bfe71759e300638196c3673c
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2024-05-22 12:01:47 +0700

    fix
    email format setting

--- src/app/auth/register.jsx
+++ src/app/auth/register.jsx
@@ -11,18 +11,33 @@ export function Register() {
     Cpassword: "",
   });
   const [log, setLog] = useState("");
+  const [isKmutt, setIsKmutt] = useState(false);
+  const allowedDomains = ["mail.kmutt.ac.th"];
 
   const handleChange = (e) => {
     setValue({
       ...value,
       [e.target.name]: e.target.value,
     });
+    const email = e.target.value;
+    const regex = /@(.+)$/;
+    const domain = email.match(regex)[1];
+
+    if (!allowedDomains.includes(domain)) {
+      setIsKmutt(false);
+    } else {
+      setLog(""); // Clear error message
+      setIsKmutt(true);
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (value.password != value.Cpassword) {
       setLog("รหัสผ่านไม่ตรงกัน");
+    }
+    if (!isKmutt) {
+      setLog("อีเมลของท่านต้องลงท้ายด้วย @mail.kmutt.ac.th");
     } else {
       axios
         .post("http://localhost:5000/api/register", value)
@@ -49,7 +64,12 @@ export function Register() {
         <label>lastname</label>
         <input type="text" name="lastname" onChange={handleChange} />
         <label>Email</label>
-        <input type="email" name="email" onChange={handleChange} />
+        <input
+          type="email"
+          name="email"
+          placeholder="email@mail.kmutt.ac.th"
+          onChange={handleChange}
+        />
         <label>Password</label>
         <input type="password" name="password" onChange={handleChange} />
         <label>Confirm Password</label>
